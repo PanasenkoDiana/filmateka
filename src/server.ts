ddec6d1b219c3f2b5726e7a95643c949456787fa
PanasenkoDiana <shad0wdrag0n19902@gmail.com>
2024-11-11 19:13:06 +0200

    server.js -> server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-const express = require('express');
+import express, { Express, Request, Response, Router } from 'express'
 
 const app = express();
 const HOST = 'localhost';
 const PORT = 8000;
 
 app.get('/', (req, res) => {
-    res.send('123');
+    res.send("13123123");
 });
 
 app.listen(PORT, HOST, () => {
