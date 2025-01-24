72e10341fad3068828edc41f36eca48559c8abfb
Misha Fatuev <mishafatuev@gmail.com>
2025-01-24 22:04:30 +0200

    api

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express, { Express, Request, Response, Router } from 'express'
+import { prisma } from "../prisma/prismaClient"
 
 const app = express();
 const HOST = 'localhost';
 const PORT = 8000;
 
-app.get('/', (req, res) => {
-    res.send("13123123");
+async function getFilms() {
+    const films = await prisma.film.findMany()
+    console.log(films)
+    return films
+}
+
+app.get('/', async (req, res) => {
+    res.json(await getFilms());
 });
 
 app.listen(PORT, HOST, () => {
